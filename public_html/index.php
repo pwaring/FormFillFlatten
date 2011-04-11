69cc94cb98d41a29e0889e05a617231c3407b023
Paul Waring <paul@xk7.net>
2011-04-11 20:42:56 +0100

    Move temporary directory location into a constant

--- public_html/index.php
+++ public_html/index.php
@@ -1,15 +1,16 @@
 <?php
 
 define('JAVA_PATH', '/usr/bin/java');
+define('TMP_DIR', '/tmp');
 define('FFF_PATH', dirname(dirname(__FILE__)));
 define('CLASSPATH', FFF_PATH . '/itext.jar:' . FFF_PATH . '/json-org.jar:' . FFF_PATH . '/');
 
 $template_tmp = isset($_FILES['template']['tmp_name']) ? $_FILES['template']['tmp_name'] : '';
 $config_tmp = isset($_FILES['config']['tmp_name']) ? $_FILES['config']['tmp_name'] : '';
 
-$template = tempnam('/tmp', 'FFF');
-$config = tempnam('/tmp', 'FFF');
-$output = tempnam('/tmp', 'FFF');
+$template = tempnam(TMP_DIR, 'FFF');
+$config = tempnam(TMP_DIR, 'FFF');
+$output = tempnam(TMP_DIR, 'FFF');
 
 // Move uploaded files, otherwise they may be deleted before we call FFF
 move_uploaded_file($template_tmp, $template);
